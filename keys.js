b11529b342842c46b493e06bdeda3fb833a2f20b
Jon Caviness <Jon@Jons-Mac.local>
2017-09-24 01:08:10 -0600

    adding packages

--- keys.js
+++ keys.js
@@ -1,10 +1,10 @@
 console.log('this is loaded');
 
 var twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: 'z1ceso3zbXxjbjgRXyQXNLxph',
+  consumer_secret: 'iA5hlEDScdObTnsDSTHZYJg7DBn6cKCWkLLBT4bZjXB5FCyxPD',
+  access_token_key: '909532430867996672-CllHxI5sJPokjzEh8ocqhyxTZClRjA3',
+  access_token_secret: 'pcsqbxAIkWOCNcG9FWEcpxsSeNEAfEOyN1gOuipstWBIU'
 };
 
 module.exports = twitterKeys;
--- liri.js
+++ liri.js
@@ -1,6 +1,7 @@
-var twitter = require('twitter');
-
 var twitterKeys = require('./keys.js');
+
+var Twitter = require('twitter');
+ 
 
 // my-tweets
 // spotify-this-song
@@ -30,16 +31,27 @@ inquirer.prompt([
 .then(function (inquirerResponse) {
 		if(inquirerResponse.password == password) {
 			console.log(`Welcome, Jon!`);
+
+			if (inquirerResponse.action === 'my-tweets') {
+			var client = new Twitter({
+				consumer_key: twitterKeys.consumer_key,
+				consumer_secret: twitterKeys.consumer_secret,
+				access_token_key: twitterKeys.access_token_key,
+				access_token_secret: twitterKeys.access_token_secret
+			});
+
+			var params = {screen_name: 'vacnoj'};
+			client.get('statuses/user_timeline', params, function(error, tweets, response) {
+debugger;
+				if (!error) {
+					for (var i = 0; i < tweets.length && i < 20; i++) {
+						console.log(`created: ${tweets[i].created_at}`);
+						console.log(`tweet: ${tweets[i].text}`);
+					}
+			  	} else console.log("error: ", error);
+			});			
+			}
 		} else console.log("Access Denied");
 
-		if (inquirerResponse.action === 'my-tweets') {
-			request("http://api.twitter.com/1/statuses/update.json", function(error, response, body) {
-				console.log('error:', error);
-  
-  				console.log('statusCode:', response && response.statusCode); 
-  
-  				console.log('body:', body); 
 
-			});
-		}
 	});
